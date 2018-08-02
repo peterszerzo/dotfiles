691bb1e0961212489715f887caa03acd7679541b
Peter Szerzo <szerzo.peter@gmail.com>
2018-08-02 13:31:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,13 @@
 cd ~
+
+# Cleanup
+rm -rf dotfiles .zshrc .vimrc .tmux.conf
+
 git clone git@github.com:peterszerzo/dotfiles.git
 cd dotfiles
 git clone git@github.com:rupa/z.git
+
+# Set up symlinks
 cd ~
 ln -sv ~/dotfiles/.zshrc ~
 ln -sv ~/dotfiles/.vimrc ~
