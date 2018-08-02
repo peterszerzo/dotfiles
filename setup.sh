d553f8f5385abb3b6abe6397b3156c0528790fdb
Peter Szerzo <szerzo.peter@gmail.com>
2018-08-02 13:22:31 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 cd ~
 git clone git@github.com:peterszerzo/dotfiles.git
+cd dotfiles
+git clone git@github.com:rupa/z.git
+cd ~
 ln -sv ~/dotfiles/.zshrc ~
 ln -sv ~/dotfiles/.vimrc ~
 ln -sv ~/dotfiles/.tmux.conf ~
