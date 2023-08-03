94ed6e756b9b34d4fb07dd242bd2d5b3bebadb01
Peter Szerzo <szerzo.peter@gmail.com>
2023-08-03 15:17:31 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -9,7 +9,8 @@ vim.opt.wrap = false
 vim.opt.inccommand = "split"
 vim.opt.swapfile = false
 vim.opt.relativenumber = true
-vim.opt.foldmethod = "syntax"
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
 vim.opt.termguicolors = true
 vim.opt.colorcolumn = "100"
 vim.opt.autoindent = true
@@ -34,8 +35,10 @@ vim.api.nvim_set_keymap("i", "jk", "<Esc>", {});
 vim.api.nvim_set_keymap("n", "<Leader>x", ":q!<CR>", {});
 vim.api.nvim_set_keymap("n", "<Leader>s", ":w<CR>", {});
 vim.api.nvim_set_keymap("n", "<Leader>h", ":noh<CR>", {});
-vim.api.nvim_set_keymap("n", "[z", "zm", {});
-vim.api.nvim_set_keymap("n", "]z", "zr", {});
+vim.api.nvim_set_keymap("n", "<Leader>r", ":LspRestart<CR>", {});
+vim.api.nvim_set_keymap("n", "<Leader>f", ":!elm-format % --yes<CR>", {});
+vim.api.nvim_set_keymap("n", "[z", "zM", {});
+vim.api.nvim_set_keymap("n", "]z", "zR", {});
 
 -- Package manager
 
@@ -61,6 +64,8 @@ require("lazy").setup({
   "tpope/vim-fugitive",
   "tpope/vim-rhubarb",
   "tpope/vim-commentary",
+  "tommcdo/vim-exchange",
+  "BurntSushi/ripgrep",
   {
     "VonHeikemen/lsp-zero.nvim",
     branch = "v2.x",
@@ -79,6 +84,23 @@ require("lazy").setup({
   {
     "nvim-telescope/telescope.nvim", tag = "0.1.2",
      dependencies = { "nvim-lua/plenary.nvim" }
+  },
+  {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+      require("copilot").setup({
+        suggestion = { enabled = false },
+        panel = { enabled = false },
+      })
+    end,
+  },
+  {
+    "zbirenbaum/copilot-cmp",
+    config = function ()
+      require("copilot_cmp").setup()
+    end
   }
 })
 
@@ -96,10 +118,9 @@ lsp.ensure_installed({
   "tsserver"
 })
 
+
 -- (Optional) Configure lua language server for neovim
 require("lspconfig").lua_ls.setup(lsp.nvim_lua_ls())
-
-lsp.setup()
 
 -- LSP zero Elm
 
@@ -118,18 +139,68 @@ end
 
 require("lspconfig").my_elm.setup({})
 
+lsp.format_on_save({
+  format_opts = {
+    async = false,
+    timeout_ms = 10000,
+  },
+  servers = {
+    ["my_elm"] = {"elm"},
+  }
+})
+
+-- Autocomplete 
+
+local cmp = require('cmp')
+local cmp_select = {behavior = cmp.SelectBehavior.Select}
+local cmp_mappings = lsp.defaults.cmp_mappings({
+  ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<CR>"] = cmp.mapping.confirm({ select = false })
+})
+
+lsp.setup_nvim_cmp({ 
+  mapping = cmp_mappings,
+  sources = {
+    { name = "copilot", group_index = 2 },
+    { name = 'path'},
+    { name = 'nvim_lsp'},
+    { name = 'buffer', keyword_length = 3},
+    { name = 'luasnip', keyword_length = 2},
+  }
+})
+
+lsp.setup()
+
 -- Tree Sitter
 
-require("lazy").setup({{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}})
+require("lazy").setup({
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function () 
+      local configs = require("nvim-treesitter.configs")
+
+      configs.setup({
+          ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "elixir", "heex", "javascript", "html", "elm" },
+          sync_install = false,
+          highlight = { enable = true, additional_vim_regex_highlighting = false },
+          indent = { enable = true },  
+        })
+    end
+  }
+})
 
 -- Other stuff
 
-vim.cmd("colorscheme tokyonight")
+vim.cmd("colorscheme tokyonight-night")
 
 builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<Leader>pf", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set("n", "<Leader>ps", function()
-  builtin.grep_string({ search = vim.fn.input("Grep > ") });
+  builtin.grep_string({ search = vim.fn.input("Grep > ") })
 end)
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
