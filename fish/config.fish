cedd4a2f5f90cc21bcf0742f64f64a5219579fe1
Peter Szerzo <szerzo.peter@gmail.com>
2025-12-11 18:36:28 +0100

    Remove codecompanion

--- fish/config.fish
+++ fish/config.fish
@@ -8,3 +8,4 @@ set PATH /usr/local/bin /usr/bin /bin /usr/sbin /sbin /opt/homebrew/bin /opt/hom
 set -q GHCUP_INSTALL_BASE_PREFIX[1]; or set GHCUP_INSTALL_BASE_PREFIX $HOME ; set -gx PATH $HOME/.cabal/bin $PATH /Users/peterszerzo/.ghcup/bin # ghcup-env
 
 # ssh-add --apple-use-keychain ~/.ssh/id_ed25519
+export PATH="$HOME/.local/bin:$PATH"
